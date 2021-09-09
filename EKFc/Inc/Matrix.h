6bb2751a08974e8a3a42d811c37789b138d09fa4
ven0m-hue <80598984+ven0m-hue@users.noreply.github.com>
2021-09-09 17:37:35 +0530

    Update Matrix.h

--- EKFc/Inc/Matrix.h
+++ EKFc/Inc/Matrix.h
@@ -9,15 +9,14 @@
 #ifndef INC_MATRIX_H_
 #define INC_MATRIX_H_
 
-
 #include "stdlib.h"
-#include "Math.h"
+#include "math.h"
 
 #define gyro_noise 1e-5
 #define accel_noise 1e-1
 
 //Handle Struct for the Matrix
-typedef struct Mat
+typedef struct
 {
 	double* entries;
 	int row;
@@ -26,310 +25,68 @@ typedef struct Mat
 }Mat;
 
 //Initialize the matrix with the custom values
-Mat* newmat_up(int r,int c,double *d){
-	Mat* M=(Mat*)malloc(sizeof(Mat));
-	M->row=r;M->col=c;
-	M->entries=(double*)malloc(sizeof(double)*r*c);
-	int k=0;
-	int l = 0;
-	for(int i=1;i<=M->row;i++){
-		for(int j=1;j<=M->col;j++){
-			M->entries[k++]=d[l++];
-		}
-	}
-	return M;
-}
-//Universal Helper Function
-Mat* newmat(int r,int c,double d){
-	Mat* M=(Mat*)malloc(sizeof(Mat));
-	M->row=r;M->col=c;
-	M->entries=(double*)malloc(sizeof(double)*r*c);
-	int k=0;
-	for(int i=1;i<=M->row;i++){
-		for(int j=1;j<=M->col;j++){
-			M->entries[k++]=d;
-		}
-	}
-	return M;
-}
+Mat* newmat_up(int r,int c,double *d);
 
+//Universal Helper Function
+Mat* newmat(int r,int c,double d);
 
 //Free Allocation
-void freemat(Mat* A){
-	free(A->entries);
-	free(A);
-}//Once the computations are over.
-
-
+void freemat(Mat* A);//Once the computations are over.
 
 //Diagonal Identity matrix
-Mat* eye(int n)
-{
-	Mat* I = newmat(n,n,0);
-	for(int i = 1;i <= n; i++)
-	{
-		I->entries[(i-1)*n+i-1]=1;
-	}
-	return I;
-}
+Mat* eye(int n);
 
 //Zero Matrix
-Mat* zeros(int r,int c){
-	Mat* Z=newmat(r,c,0);
-	return Z;
-}
+Mat* zeros(int r,int c);
 
 //Ones Matrix
-Mat* ones(int r,int c){
-	Mat* O=newmat(r,c,1);
-	return O;
-}
+Mat* ones(int r,int c);
 
 //Output a value at an index M[i][j]
-double get(Mat* M,int r,int c){
-	double d=M->entries[(r-1)*M->col+c-1];
-	return d;
-}
+double get(Mat* M,int r,int c);
 
 //Multiply the matrix with the scaler multiples
-Mat* scalermultiply(Mat* M,double c){
-	Mat* B=newmat(M->row,M->col,0);
-	int k=0;
-	for(int i=0;i<M->row;i++){
-		for(int j=0;j<M->col;j++){
-			B->entries[k]=M->entries[k]*c;
-			k+=1;
-		}
-	}
-	return B;
-}
+Mat* scalermultiply(Mat* M,double c);
 
 //Summation Matrix
-Mat* sum(Mat* A,Mat* B){
-	int r=A->row;
-	int c=A->col;
-	Mat* C=newmat(r,c,0);
-	int k=0;
-	for(int i=0;i<r;i++){
-		for(int j=0;j<c;j++){
-			C->entries[k]=A->entries[k]+B->entries[k];
-			k+=1;
-		}
-	}
-	return C;
-}
-
+Mat* sum(Mat* A,Mat* B);
 //Subtraction Matrix
-Mat* minus(Mat* A,Mat* B){
-	int r=A->row;
-	int c=A->col;
-	Mat* C=newmat(r,c,0);
-	int k=0;
-	for(int i=0;i<r;i++){
-		for(int j=0;j<c;j++){
-			C->entries[k]=A->entries[k]-B->entries[k];
-			k+=1;
-		}
-	}
-	return C;
-}
+Mat* minus(Mat* A,Mat* B);
 
 //Multiply the matrix A and B --> Very Very important function to make your life easier.
-Mat* multiply(Mat* A,Mat* B){
-	int r1=A->row;
-	int r2=B->row;
-	int c1=A->col;
-	int c2=B->col;
-	if (r1==1&&c1==1){
-		Mat* C=scalermultiply(B,A->entries[0]);
-		return C;
-	}else if (r2==1&&c2==1){
-		Mat* C=scalermultiply(A,B->entries[0]);
-		return C;
-	}
-	Mat* C=newmat(r1,c2,0);
-	for(int i=1;i<=r1;i++){
-		for(int j=1;j<=c2;j++){
-			double de=0;
-			for(int k=1;k<=r2;k++){
-				de+=A->entries[(i-1)*A->col+k-1]*B->entries[(k-1)*B->col+j-1];
-			}
-			C->entries[(i-1)*C->col+j-1]=de;
-		}
-	}
-	return C;
-}
+Mat* multiply(Mat* A,Mat* B);//Helper function
 
-//Helper function
-Mat* removerow(Mat* A,int r){
-	Mat* B=newmat(A->row-1,A->col,0);
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			if(i!=r){
-				B->entries[k]=A->entries[(i-1)*A->col+j-1];
-				k+=1;
-			}
-		}
-	}
-	return B;
-}
+Mat* removerow(Mat* A,int r);
+//Helper Function
+
+Mat* removecol(Mat* A,int c);
 
 //Helper Function
-Mat* removecol(Mat* A,int c){
-	Mat* B=newmat(A->row,A->col-1,0);
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			if(j!=c){
-				B->entries[k]=A->entries[(i-1)*A->col+j-1];
-				k+=1;
-			}
-		}
-	}
-	return B;
-}
-
+void removerow2(Mat* A,Mat* B,int r);
 //Helper Function
-void removerow2(Mat* A,Mat* B,int r){
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			if(i!=r){
-				B->entries[k++]=A->entries[(i-1)*A->col+j-1];
-			}
-		}
-	}
-}
 
-//Helper Function
-void removecol2(Mat* A,Mat* B,int c){
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			if(j!=c){
-				B->entries[k++]=A->entries[(i-1)*A->col+j-1];
-			}
-		}
-	}
-}
+void removecol2(Mat* A,Mat* B,int c);
 
 //Transpose of the Matrix
-Mat* transpose(Mat* A){
-	Mat* B=newmat(A->col,A->row,0);
-	int k=0;
-	for(int i=1;i<=A->col;i++){
-		for(int j=1;j<=A->row;j++){
-			B->entries[k]=A->entries[(j-1)*A->row+i-1];
-			k+=1;
-		}
-	}
-	return B;
-}
+Mat* transpose(Mat* A);
 
 //Determinant
-double det(Mat* M){
-	int r=M->row;
-	int c=M->col;
-	if(r==1&&c==1){
-		double d=M->entries[0];
-		return d;
-	}
-	Mat* M1=removerow(M,1);
-	Mat* M2=newmat(M->row-1,M->col-1,0);
-	double d=0, si=+1;
-	for(int j=1;j<=M->col;j++){
-		double c=M->entries[j-1];
-		removecol2(M1,M2,j);
-		d+=si*det(M2)*c;
-		si*=-1;
-	}
-	freemat(M1);
-	freemat(M2);
-	return d;
-}
+double det(Mat* M);
 
 //Adjoint of a matrix to compute the future inverse of a matrix
-Mat* adjoint(Mat* A){
-	Mat* B=newmat(A->row,A->col,0);
-	Mat* A1=newmat(A->row-1,A->col,0);
-	Mat* A2=newmat(A->row-1,A->col-1,0);
-	for(int i=1;i<=A->row;i++){
-		removerow2(A,A1,i);
-		for(int j=1;j<=A->col;j++){
-			removecol2(A1,A2,j);
-			double si=pow(-1,(double)(i+j));
-			B->entries[(i-1)*B->col+j-1]=det(A2)*si;
-		}
-	}
-	Mat* C=transpose(B);
-	freemat(A1);
-	freemat(A2);
-	freemat(B);
-	return C;
-}
+Mat* adjoint(Mat* A);
 
 //The Inverse of a matrix
-Mat* inverse(Mat* A){
-	Mat* B=adjoint(A);
-	double de=det(A);
-	Mat* C=scalermultiply(B,1/de);
-	freemat(B);
-	return C;
-}
+Mat* inverse(Mat* A);
 
 //Horizontal Concatenate
-Mat* hconcat(Mat* A,Mat* B){
-	Mat* C=newmat(A->row,A->col+B->col,0);
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			C->entries[k]=A->entries[(i-1)*A->col+j-1];
-			k++;
-		}
-		for(int j=1;j<=B->col;j++){
-			C->entries[k]=B->entries[(i-1)*B->col+j-1];
-			k++;
-		}
-	}
-	return C;
-}
+Mat* hconcat(Mat* A,Mat* B);
 
 //Vertical Concatenate
-Mat* vconcat(Mat* A,Mat* B){
-	Mat* C=newmat(A->row+B->row,A->col,0);
-	int k=0;
-	for(int i=1;i<=A->row;i++){
-		for(int j=1;j<=A->col;j++){
-			C->entries[k]=A->entries[(i-1)*A->col+j-1];
-			k++;
-		}
-	}
-	for(int i=1;i<=B->row;i++){
-		for(int j=1;j<=B->col;j++){
-			C->entries[k]=B->entries[(i-1)*B->col+j-1];
-			k++;
-		}
-	}
-	return C;
-}
-
+Mat* vconcat(Mat* A,Mat* B);
 
 //Custom Functions
-Mat* eye_Q(int n){
-	Mat* I=newmat(n,n,0);
-	for(int i=1;i<=n;i++){
-		I->entries[(i-1)*n+i-1]= gyro_noise;
-	}
-	return I;
-}
+Mat* eye_Q(int n);
 
-Mat* eye_R(int n){
-	Mat* I=newmat(n,n,0);
-	for(int i=1;i<=n;i++){
-		I->entries[(i-1)*n+i-1]= accel_noise;
-	}
-	return I;
-}
-
+Mat* eye_R(int n);
 #endif /* INC_MATRIX_H_ */
